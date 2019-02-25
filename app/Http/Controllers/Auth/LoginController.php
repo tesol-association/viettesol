4a87c000e7e498dc314dd7d418fbf17868c7dd09
thangbk111 <nguyenvanthang2911@gmail.com>
2019-02-25 13:49:02 +0700

    register and login logout done

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/demo-main';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'user_name',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'affiliation',
+        'email',
+        'password',
     ];
 
     /**
