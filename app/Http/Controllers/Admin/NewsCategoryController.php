96f9765231d080346e046dc265fb01e786b1f492
thangbk111 <nguyenvanthang2911@gmail.com>
2019-03-04 13:30:07 +0700

    fix bug list news

---
+++ app/Http/Controllers/Admin/NewsCategoryController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\NewsCategory;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class NewsCategoryController extends Controller
+{
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $newsCategory = NewsCategory::all();
+        return view('layouts.admin.news_category.list', ['newsCategory' => $newsCategory]);
+    }
+
+    public function create()
+    {
+        return view('layouts.admin.news_category.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = $this->validateData($request->all());
+        if ($validator->fails()) {
+            return redirect()
+                ->route('admin_news_category_create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+        $newsCategory = new NewsCategory();
+        $newsCategory->name = $request->name;
+        $newsCategory->slug = $request->slug;
+        $newsCategory->description = $request->description;
+        $newsCategory->save();
+        return redirect()->route('admin_news_category_list')->with('success', 'Create ' . $newsCategory->name . ' successful !');
+    }
+
+    public function edit($id)
+    {
+        $newCategory = NewsCategory::find($id);
+        return view('layouts.admin.news_category.edit',['newCategory'=> $newCategory]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $data["id"] = $id;
+        $validator = $this->validateData($data);
+        if ($validator->fails()) {
+            return redirect()
+                ->route('admin_news_category_edit', ["id" => $id])
+                ->withErrors($validator)
+                ->withInput();
+        }
+        $newsCategory = NewsCategory::find($id);
+        $newsCategory->name = $request->name;
+        $newsCategory->slug = $request->slug;
+        $newsCategory->description = $request->description;
+        $newsCategory->save();
+        return redirect()->route('admin_news_category_list')->with('success', 'Update ' . $newsCategory->name . ' successful !');
+    }
+
+    public function destroy($id)
+    {
+        $newsCategory = NewsCategory::find($id);
+        NewsCategory::destroy($id);
+        return redirect()->route('admin_news_category_list')->with('success','Xóa thành công: ' . $newsCategory->name . '!');
+    }
+
+    public function validateData($data) {
+        $id = isset($data["id"]) && $data["id"] ? $data["id"] : null;
+        return Validator::make($data, [
+            'name' => 'required',
+            'slug' => 'required|unique:new_categories,slug,' . $id,
+        ]);
+    }
+}
