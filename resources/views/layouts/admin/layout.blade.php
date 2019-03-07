ef74ebe94f8a38512010d57561f287e7b6d44456
Doan Phu <doanphua4@gmail.com>
2019-03-07 14:03:13 +0700

    Contact Form

--- resources/views/layouts/admin/layout.blade.php
+++ resources/views/layouts/admin/layout.blade.php
@@ -321,6 +321,16 @@ desired effect
           </ul>
         </li>
         <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Contact Form Management</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="{{ route('admin_contactForm_list') }}">Contact Form List</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
           <a href="#"><i class="fa fa-link"></i> <span>Category Management</span>
             <span class="pull-right-container"></span>
           </a>
