8e777c21e7392fb605d473135b8565efc231f10c
thangbk111 <nguyenvanthang2911@gmail.com>
2019-03-08 14:58:56 +0700

    Track and Review Form done

---
+++ resources/views/layouts/admin/criteria_review/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin.conference_layout')
+@section('title','Edit Track')
+@section('css')
+@endsection
+@section('content')
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Edit Track {{ $track->name }} For Conference: {{ $conference->title }}</h3>
+            <div class="box-tools pull-right">
+                <a href="{{ route('admin_track_list', ["id" => $conference->id]) }}" class="btn btn-block btn-info"><i class="fa fa-backward"></i> Track List</a>
+            </div>
+        </div>
+
+        <div class="box-body">
+            <!-- form start -->
+            <form  method="post" action="{{ route('admin_track_update', ["conference_id" => $conference->id, "id" => $track->id]) }}" enctype="multipart/form-data">
+                @csrf
+                <div class="box-body">
+                    <div class="form-group {{ $errors->first('name') ? 'has-error' : ''}}">
+                        <label for="name">Name*</label>
+                        <input id="name" type="text" class="form-control" placeholder="Enter Title" name="name" required value="{{ $track->name }}">
+                        @if ($errors->has('name'))
+                            <span class="help-block">{{ $errors->first('name') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="form-group {{ $errors->first('abbrev') ? 'has-error' : ''}}">
+                        <label for="abbrev">Abbrev*</label>
+                        <input id="abbrev" type="text" class="form-control" placeholder="Enter Title" name="abbrev" required value="{{ $track->abbrev }}">
+                        @if ($errors->has('abbrev'))
+                            <span class="help-block">{{ $errors->first('abbrev') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="form-group {{ $errors->first('policy') ? 'has-error' : ''}}">
+                        <label for="policy">Policy</label>
+                        <textarea name="policy" id="policy" class="form-control" rows="3" placeholder="Enter Policy ...">{{ $track->policy }}</textarea>
+                        @if ($errors->has('policy'))
+                            <span class="help-block">{{ $errors->first('policy') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="form-group {{ $errors->first('description') ? 'has-error' : ''}}">
+                        <label for="description">Description</label>
+                        <textarea name="description" id="description" class="form-control" rows="3" placeholder="Enter Description ...">{{ $track->description }}</textarea>
+                        @if ($errors->has('description'))
+                            <span class="help-block">{{ $errors->first('description') }}</span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+            <!-- form end -->
+        </div>
+    </div>
+@endsection
+@section('js')
+@endsection
---
+++ resources/views/layouts/admin/review_form/create.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin.conference_layout')
+@section('title','Create Criteria Review')
+@section('css')
+@endsection
+@section('content')
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Create Criteria Review</h3>
+            <div class="box-tools pull-right">
+                <a href="{{ route('admin_criteria_review_list', ["conference_id" => $conference->id]) }}" class="btn btn-block btn-info"><i class="fa fa-backward"></i> Criteria Review List</a>
+            </div>
+        </div>
+
+        <div class="box-body">
+            <!-- form start -->
+            <form  method="post" action="{{ route('admin_criteria_review_store', ["conference_id" => $conference->id]) }}" enctype="multipart/form-data">
+                @csrf
+                <div class="box-body">
+                    <div class="form-group {{ $errors->first('name') ? 'has-error' : ''}}">
+                        <label for="name">Name*</label>
+                        <input id="name" type="text" class="form-control" placeholder="Enter Title" name="name" required value="{{ old('name') }}">
+                        @if ($errors->has('name'))
+                            <span class="help-block">{{ $errors->first('name') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="form-group {{ $errors->first('possible_values') ? 'has-error' : ''}}">
+                        <label>Possible Values*</label>
+                        <input type="text" class="form-control possible_values" placeholder="Each Value is separated by commas. Example: 1, 2, 3, 4, 5" name="possible_values" required>
+                        @if ($errors->has('possible_values'))
+                            <span class="help-block">{{ $errors->first('possible_values') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="form-group {{ $errors->first('description') ? 'has-error' : ''}}">
+                        <label for="description">Description</label>
+                        <textarea name="description" id="description" class="form-control" rows="3" placeholder="Enter Description ...">{{ old('description') }}</textarea>
+                        @if ($errors->has('description'))
+                            <span class="help-block">{{ $errors->first('description') }}</span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+            <!-- form end -->
+        </div>
+    </div>
+@endsection
+@section('js')
+@endsection
