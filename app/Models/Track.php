5b78e7a33f8ba18a6d7e63b2e60ce40899f87605
thangbk111 <nguyenvanthang2911@gmail.com>
2019-03-11 12:11:38 +0700

    conference

---
+++ app/Models/Track.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Track extends Model
+{
+    protected $fillable = [
+        'name',
+        'abbrev',
+        'description',
+        'policy',
+        'conference_id'
+    ];
+
+    public function papers()
+    {
+        return $this->hasMany('App\Models\Paper', 'track_id');
+    }
+
+    public function conference()
+    {
+        return $this->belongsTo('App\Models\Conference', 'conference_id');
+    }
+}
