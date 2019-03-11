b8f577f5c9b38c94bdd26076be12fd4acb8d5a33
NamTong1998 <nam.th.vnist@gmail.com>
2019-03-11 12:49:49 +0700

    ...

--- resources/views/layouts/admin/contact_type/list.blade.php
+++ resources/views/layouts/admin/contact_type/list.blade.php
@@ -3,6 +3,11 @@
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('admin/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
+<style type="text/css">
+    td{
+        text-align: center;
+    }
+</style>
 @endsection
 
 @section('page-header')
