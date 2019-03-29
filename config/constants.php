58f0751f2a0ffb7daf6556ea4b6e5054d4daca60
thangbk111 <nguyenvanthang2911@gmail.com>
2019-03-29 11:42:10 +0700

    change review_assignment table

---
+++ config/constants.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thangbk111
+ * Date: 3/28/2019
+ * Time: 6:07 PM
+ */
+return [
+    'CONFERENCE_ROLE' => [
+        'DIRECTOR' => 'Director',
+        'TRACK_DIRECTOR' => 'Track Director',
+        'REVIEWER' => 'Reviewer',
+        'AUTHOR' => 'Author'
+    ],
+    'REVIEW_ASSIGNMENT' => [
+        'INDEX_ASSIGNMENT' => ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+        'ACCEPTED_ASSIGNMENT' => 0,
+        'REJECTED_ASSIGNMENT' => 0,
+        'ACCEPT_RECOMMENDATION' => 0,
+        'REVISION_RECOMMENDATION' => 0,
+        'REJECT_RECOMMENDATION' => 1,
+    ],
+];
--- database/mysql/update_20190318_paper.sql
+++ database/mysql/update_20190318_paper.sql
@@ -4,8 +4,7 @@ ALTER TABLE `paper_authors` ADD `seq` INT NOT NULL AFTER `country`;
 ALTER TABLE `paper_authors` CHANGE `seq` `seq` INT(11) NOT NULL DEFAULT '0';
 ALTER TABLE `session_types` CHANGE `description` `description` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
 ALTER TABLE `papers` ADD `session_type_id` INT NOT NULL AFTER `track_id`;
---- 19/03/2019
-RENAME TABLE paper_authors to authors;
+--- 19/03/2019 -----
 RENAME TABLE paper_authors to authors;
 ALTER TABLE `authors` DROP `seq`;
 
@@ -22,9 +21,16 @@ ENGINE = InnoDB;
 ALTER TABLE `papers` DROP `author_id`;
 ALTER TABLE `papers` DROP `co_authors`;
 
----- 20/03/2019
+---- 20/03/2019 ----
 DROP TABLE ` review_form_setting`;
 ALTER TABLE `review_form` CHANGE `attach_file` `attach_file` VARCHAR(255) NULL DEFAULT NULL;
 ALTER TABLE `criteria_review` ADD `review_form_id` INT NOT NULL AFTER `id`;
----- 21/03/2019
+---- 21/03/2019 -----
 ALTER TABLE `tracks` ADD `review_form_id` INT NULL AFTER `conference_id`;
+
+---- 27/03/2019 -----
+ALTER TABLE `review_assignments` CHANGE `data` `reviewer_response` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `review_assignments` ADD `total` INT NULL AFTER `cancelled`;
+ALTER TABLE `review_assignments` ADD `comment` TEXT NULL AFTER `cancelled`;
+ALTER TABLE `review_assignments` DROP `review_form_id`;
+ALTER TABLE `papers` ADD `submission_by` INT NOT NULL AFTER `submission_progress`;
